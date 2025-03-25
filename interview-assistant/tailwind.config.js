a6f71c03dbce4d43e41815692aaa13cc00c55abb
NuvaGit <jack.neilan@ucdconnect.ie>
2025-03-25 20:56:23 +0000

    gg

--- interview-assistant/tailwind.config.js
+++ interview-assistant/tailwind.config.js
@@ -1,25 +1,25 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: {
-//           500: '#0073ff',
-//           600: '#005cd9',
-//         },
-//         secondary: {
-//           500: '#69affa',
-//           600: '#4a8ce6',
-//         },
-//         accent: {
-//           500: '#e090ff',
-//           600: '#c26fec',
-//         },
-//       },
-//     },
-//   },
-// };
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: '#0073ff',
+          600: '#005cd9',
+        },
+        secondary: {
+          500: '#69affa',
+          600: '#4a8ce6',
+        },
+        accent: {
+          500: '#e090ff',
+          600: '#c26fec',
+        },
+      },
+    },
+  },
+};
